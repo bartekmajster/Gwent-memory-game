1480152404e96a48c3a122ba4e5f4247ad5290ed
Bartłomiej Kochanowicz <bartlomiej.kochanowicz@gmail.com>
2020-02-28 19:27:03 +0100

    fix image problem

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gwent memory game
+
+#### Game written in jQuery to practice this library.
+
+### DEMO:
+- [Gwent game](https://bartekmajster.github.io/Gwent-memory-game/production/index.html)
+
+#### To do:
+- add mobile view
+- fix image path problem on production
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,15 @@ import generateBoard from './modules/generateBoard';
 const board = generateBoard();
 const cards = $('.board__card');
 
+let prevCard;
+
 function showCard() {
   const [index, item, box] = [this.id, $(this).find('.cont__back'), $(this).find('.creation')];
-  item.css('background', `url('../../images/characters/${board[index]}.png')`);
+  item.css('background', `url('../images/characters/${board[index]}.png')`);
   box.toggleClass('creation--active');
+  prevCard = board[index];
 }
 
 cards.on('click', showCard);
+
+console.log(prevCard);
